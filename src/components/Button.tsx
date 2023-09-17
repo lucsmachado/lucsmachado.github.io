aad91e1a165c8019e3a7a4b7439815b9683304c0
Lucas Machado <lcmachado.cs@gmail.com>
2023-09-17 14:02:17 -0300

    added React and Tailwind integrations

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import type { HTMLAttributes } from "react";
+
+type Props = HTMLAttributes<HTMLButtonElement> & {
+
+}
+
+export default function Button({ children, ...rest }: Props) {
+  return (
+    <button {...rest}>{children}</button>
+  );
+}
---
+++ src/components/Main.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <h1 className="text-3xl text-white">Hi there! My name is Lucas</h1>
+    </main>
+  );
+}
