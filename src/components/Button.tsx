6296b7bbc92a07c033d8e1a23cb7acbc3b5ab79e
Lucas Machado <lcmachado.cs@gmail.com>
2023-12-19 15:38:58 -0300

    feat: v1

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,7 @@
 import type { HTMLAttributes } from "react";
 
-type Props = HTMLAttributes<HTMLButtonElement> & {
-
-}
+type Props = HTMLAttributes<HTMLButtonElement> & {};
 
 export default function Button({ children, ...rest }: Props) {
-  return (
-    <button {...rest}>{children}</button>
-  );
+  return <button {...rest}>{children}</button>;
 }
