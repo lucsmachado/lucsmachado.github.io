0963a7f0df2e4b98bbc5025555358c5e90894959
Lucas Machado <lcmachado.cs@gmail.com>
2023-12-19 16:08:43 -0300

    chore: remove unused components

--- src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-import type { HTMLAttributes } from "react";
-
-type Props = HTMLAttributes<HTMLButtonElement> & {};
-
-export default function Button({ children, ...rest }: Props) {
-  return <button {...rest}>{children}</button>;
-}
--- src/components/Main.tsx
+++
@@ -1,7 +0,0 @@
-export default function Main() {
-  return (
-    <main>
-      <h1 className="text-3xl text-white">Hi there! My name is Lucas</h1>
-    </main>
-  );
-}
